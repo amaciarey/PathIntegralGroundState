30cd38f07d78aff3517b138ed894ba9903142273
Adrian Macia <adrianmaciarey@gmail.com>
2015-06-04 15:57:52 +0200

    2015/06/04: Redefinition of the fourth order propagator.

--- vpi_mod.f90
+++ vpi_mod.f90
@@ -17,35 +17,43 @@ contains
 
     real (kind=8)    :: GreenFunction
     real (kind=8)    :: dt,Pot,F2
+    real (kind=8)    :: Ve,dVe
+    real (kind=8)    :: Vc,dVc
     integer (kind=4) :: ib,opt
 
     GreenFunction = 0.d0
 
     if (opt==0) then
 
+       Ve = Pot+a_1*dt**2*F2/6.d0
+       Vc = Pot+(1.d0-a_1)*dt**2*F2/6.d0
+
        if (ib==0) then
-          GreenFunction = dt*Pot/3.d0+2.d0*a_1*dt**3*F2/9.d0
+          GreenFunction = dt*Ve/3.d0
        else if (ib==2*Nb) then
-          GreenFunction = dt*Pot/3.d0+2.d0*a_1*dt**3*F2/9.d0
+          GreenFunction = dt*Ve/3.d0
        else
           if (mod(ib,2)==0) then
-             GreenFunction = 2.d0*(dt*Pot/3.d0+2.d0*a_1*dt**3*F2/9.d0)
+             GreenFunction = 2.d0*dt*Ve/3.d0
           else
-             GreenFunction = dt*4.d0*Pot/3.d0+(1.d0-2.d0*a_1)*dt**3*F2/9.d0
+             GreenFunction = 4.d0*dt*Vc/3.d0
           end if
        end if
 
     else if (opt==1) then
        
+       dVe = Pot+a_1*dt**2*F2/2.d0
+       dVc = Pot+(1-a_1)*dt**2*F2/2.d0
+
        if (ib==0) then
-          GreenFunction = Pot/3.d0+2.d0*a_1*dt**2*F2/3.d0
+          GreenFunction = dVe/3.d0
        else if (ib==2*Nb) then
-          GreenFunction = Pot/3.d0+2.d0*a_1*dt**2*F2/3.d0
+          GreenFunction = dVe/3.d0
        else
           if (mod(ib,2)==0) then
-             GreenFunction = 2.d0*(Pot/3.d0+2.d0*a_1*dt**2*F2/3.d0)
+             GreenFunction = 2.d0*dVe/3.d0
           else
-             GreenFunction = 4.d0*Pot/3.d0+(1.d0-2.d0*a_1)*dt**2*F2/3.d0
+             GreenFunction = 4.d0*dVc/3.d0
           end if
        end if
 
